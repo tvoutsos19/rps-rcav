78e19a42b433d098fa47c5e7e712c0fb93f3e655
Jelani Woods <jelani@firstdraft.com>
2019-10-24 11:33:46 -0500

    Starting Point

---
+++ README.md
@@ -0,0 +1,3 @@
+# RPS API
+
+This project is the companion to the [Adding Routes](https://chapters.firstdraft.com/chapters/779) Chapter.
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+
+  
+
+  # Write your routes above this line
+  # =================================
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+end
