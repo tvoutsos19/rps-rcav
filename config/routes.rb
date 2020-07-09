81f0f5a7391fedff9e6f9d886c7270792ceb349a
Jelani Woods <jelani@firstdraft.com>
2020-07-09 14:45:14 -0500

    Summer '20

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
 
-  
 
-  # Write your routes above this line
-  # =================================
-
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 end
