22459eb5b71d2e86bc8af74f227e659160f739ef
tvoutsos <tvoutsos@chicagobooth.edu>
2020-10-13 02:56:12 +0000

    1st Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    #write a ton of code
+
+    # redirect_to("https://www.wikipedia.org")
+
+    #render ({ :html => "<h1>Hello, world!</h1>".html_safe })
+
+    render ({ :template => "game_templates/user_rock.html.erb"})
+  end
+
 end
