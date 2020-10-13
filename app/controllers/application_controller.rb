dd83225cfe213907fbab8c6ae3d9998284d6e7d6
tvoutsos <tvoutsos@chicagobooth.edu>
2020-10-13 04:52:04 +0000

    3rd Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render ({ :template => "game_templates/rules.html.erb"})
+  end
+
   def play_rock
     #write a ton of code
 
@@ -12,6 +16,20 @@ class ApplicationController < ActionController::Base
     #render ({ :html => "<h1>Hello, world!</h1>".html_safe })
 
     render ({ :template => "game_templates/user_rock.html.erb"})
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+    
+    render ({ :template => "game_templates/user_paper.html.erb" })
   end
 
 end
